c96595939b7dc4b56c933544109413a836bc1044
Kyungjae Lee <kjxlee@gmail.com>
2026-03-28 16:01:21 +0100

    create '20_Semaphore_Mutex'

---
+++ workspace/20_Semaphore_Mutex/Core/Inc/uart.h
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ *
+ * @file	uart.h
+ * @brief	Interface of UART driver.
+ * @author	Kyungjae Lee
+ * @date	Aug 23, 2025
+ *
+ ******************************************************************************/
+
+#ifndef UART_H
+#define UART_H
+
+/* Includes ------------------------------------------------------------------*/
+
+/* Function Prototypes -------------------------------------------------------*/
+void USART2_UART_TX_Init(void);
+void USART2_UART_RX_Init(void);
+
+#endif /* UART_H */
